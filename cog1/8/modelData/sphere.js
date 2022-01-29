f7d97934cf1a3059ad9f56e48244109dce4aa0f2
Hicham Naoufal <7191269+hnaoufal@users.noreply.github.com>
2022-01-29 15:44:04 +0100

    Finish sphere

--- cog1/8/modelData/sphere.js
+++ cog1/8/modelData/sphere.js
@@ -19,8 +19,8 @@
  * 0      a     2
  *
  * Parameter:
- * 	recursionDepth
- * 	color or -1 = many colors
+ *  recursionDepth
+ *  color or -1 = many colors
  *
  * For texture see:
  * http://earthobservatory.nasa.gov/Features/BlueMarble/
@@ -29,160 +29,193 @@
  * @module sphere
  */
 
-define(["exports", "data", "glMatrix"], function(exports, data) {
-	"use strict";
+define(["exports", "data", "glMatrix"], function (exports, data) {
+  "use strict";
 
-	/**
-	 * Procedural calculation.
-	 *
-	 * @parameter object with fields:
-	 * @parameter scale
-	 * @parameter recursionDepth
-	 * @parameter color [-1 for many colors]
-	 */
-	exports.create = function(parameter) {
-				
-		if(parameter) {
-			var scale = parameter.scale;
-			var recursionDepth = parameter.recursionDepth;
-			var color = parameter.color;
-			var textureURL = parameter.textureURL;
-		}
-		// Set default values if parameter is undefined.
-		if(scale == undefined) {
-			scale = 250;
-		}
-		if(recursionDepth == undefined) {
-			recursionDepth = 3;
-		}
-		if(color == undefined) {
-			color = 9;
-		}
-		if(textureURL == undefined) {
-			textureURL = "";
-		}
+  /**
+   * Procedural calculation.
+   *
+   * @parameter object with fields:
+   * @parameter scale
+   * @parameter recursionDepth
+   * @parameter color [-1 for many colors]
+   */
+  exports.create = function (parameter) {
 
-		// Instance of the model to be returned.
-		var instance = {};
+    if (parameter) {
+      var scale = parameter.scale;
+      var recursionDepth = parameter.recursionDepth;
+      var color = parameter.color;
+      var textureURL = parameter.textureURL;
+    }
+    // Set default values if parameter is undefined.
+    if (scale === undefined) {
+      scale = 250;
+    }
+    if (recursionDepth === undefined) {
+      recursionDepth = 3;
+    }
+    if (color === undefined) {
+      color = 9;
+    }
+    if (textureURL === undefined) {
+      textureURL = "";
+    }
 
-		// BEGIN exercise Sphere
+    // Instance of the model to be returned.
+    var instance = {};
 
-		// Starting with octahedron vertices
+    // BEGIN exercise Sphere
 
-		// octahedron triangles
+    // Starting with octahedron vertices
+    // octahedron triangles
 
-		devide_all.call(instance, recursionDepth);
+    instance.vertices = [
+      [1.0, 0.0, 0.0],
+      [-1.0, 0.0, 0.0],
+      [0.0, 1.0, 0.0],
+      [0.0, -1.0, 0.0],
+      [0.0, 0.0, 1.0],
+      [0.0, 0.0, -1.0],
+    ];
 
-		// END exercise Sphere
-		
-		devide_all.call(instance, recursionDepth);
-
-		generateTextureCoordinates.call(instance);
-
-		data.applyScale.call(instance, scale);
-		data.setColorForAllPolygons.call(instance, color);
-
-		instance.textureURL = textureURL;
-
-		return instance;
-	}
-	/**
-	 * Called with this pointer set to instance.
-	 * Generate texture coordinates one per each corner of a polygon,
-	 * thus a vertex can have more than one uv, depending on the polygon it is part of.
-	 * The coordinates u.v represent the angles theta and phi
-	 * of point vector to x and y axis.
-	 * See:
-	 * http://tpreclik.dyndns.org/codeblog/?p=9
-	 *
-	 * Assume that vertices are not yet scaled, thus have length 1.
-	 *
-	 */
-	function generateTextureCoordinates() {
-
-		// BEGIN exercise Sphere-Earth-Texture
-
-		// As there is not exactly one texture coordinate per vertex,
-		// we have to install a different mapping as used for polygonVertices to vertices.
-		// For texture coordinate system use openGL standard, where origin is bottom-left.
-		this.textureCoord = [];
-		this.polygonTextureCoord = [];
+    instance.polygonVertices = [
+      [0, 4, 2],
+      [2, 4, 1],
+      [1, 4, 3],
+      [3, 4, 0],
+      [0, 2, 5],
+      [2, 1, 5],
+      [1, 3, 5],
+      [3, 0, 5],
+    ];
 
 
-			// Loop over vertices/edges in polygon.
+    devide_all.call(instance, recursionDepth);
 
-				// Shorthands for the current vertex.
+    // END exercise Sphere
+
+    devide_all.call(instance, recursionDepth);
+
+    generateTextureCoordinates.call(instance);
+
+    data.applyScale.call(instance, scale);
+    data.setColorForAllPolygons.call(instance, color);
+
+    instance.textureURL = textureURL;
+
+    return instance;
+  }
+
+  /**
+   * Called with this pointer set to instance.
+   * Generate texture coordinates one per each corner of a polygon,
+   * thus a vertex can have more than one uv, depending on the polygon it is part of.
+   * The coordinates u.v represent the angles theta and phi
+   * of point vector to x and y axis.
+   * See:
+   * http://tpreclik.dyndns.org/codeblog/?p=9
+   *
+   * Assume that vertices are not yet scaled, thus have length 1.
+   *
+   */
+  function generateTextureCoordinates() {
+
+    // BEGIN exercise Sphere-Earth-Texture
+
+    // As there is not exactly one texture coordinate per vertex,
+    // we have to install a different mapping as used for polygonVertices to vertices.
+    // For texture coordinate system use openGL standard, where origin is bottom-left.
+    this.textureCoord = [];
+    this.polygonTextureCoord = [];
 
 
-				// Calculate longitude (east-west position) phi (u-coordinate).
-				// arctangent (of here z/x), representing the angle theta between the positive X axis, and the point.
-				// Scale phi to uv range [0,1].
+    // Loop over vertices/edges in polygon.
+
+    // Shorthands for the current vertex.
 
 
-				// Calculate latitude (north-south position) theta (v-coordinate) from y component of vertex.
-				// Scale theta to uv range [0,1].
+    // Calculate longitude (east-west position) phi (u-coordinate).
+    // arctangent (of here z/x), representing the angle theta between the positive X axis, and the point.
+    // Scale phi to uv range [0,1].
 
 
-				// Store new uv coordinate in new uv-vector.
-				//console.log("phi:" + (~~(phi * 100)) + "  theta:" + (~~(theta * 100)) + " x:" + (~~(x * 100)) + " z:" + (~~(z * 100)));
-				
+    // Calculate latitude (north-south position) theta (v-coordinate) from y component of vertex.
+    // Scale theta to uv range [0,1].
 
-		// Problem with phi/u: phi=360 and phi=0 are the same point in 3D and also on a tiled texture.
-		// But for faces it is a difference if uv-range is 350¡-360¡ [.9-1]or 350¡-0¡ [.9-0].
-		// Thus, insert a check/hack (assuming faces cover only a small part of the texture):
 
-			// Check if u-range should be low or high (left or right in texture),
-			// by summing up u values (ignoring u=0 values).
+    // Store new uv coordinate in new uv-vector.
+    //console.log("phi:" + (~~(phi * 100)) + "  theta:" + (~~(theta * 100)) + " x:" + (~~(x * 100)) + " z:" + (~~(z * 100)));
 
-			// Check and correct u values if 0;
-		
-		// END exercise Sphere-Earth-Texture
-	}
 
-	// BEGIN exercise Sphere
+    // Problem with phi/u: phi=360 and phi=0 are the same point in 3D and also on a tiled texture.
+    // But for faces it is a difference if uv-range is 350ï¿½-360ï¿½ [.9-1]or 350ï¿½-0ï¿½ [.9-0].
+    // Thus, insert a check/hack (assuming faces cover only a small part of the texture):
 
-	/**
-	 * Recursively divide all triangles.
-	 */
-	function devide_all(recursionDepth, nbRecusions) {
-		// nbRecusions is not set from initial call.
-		if(nbRecusions == undefined) {
-			nbRecusions = 0;
-		}
-		// Stop criterion.
+    // Check if u-range should be low or high (left or right in texture),
+    // by summing up u values (ignoring u=0 values).
 
-		//console.log("nbRecusions: "+nbRecusions);
-		// Assemble divided polygons in an new array.
+    // Check and correct u values if 0;
 
-			// Indices of the last three new vertices.
+    // END exercise Sphere-Earth-Texture
+  }
 
-				// Calculate new vertex in the middle of edge.
+  // BEGIN exercise Sphere
 
-				// Check if the new vertex exists already.
-				// This happens because edges always belong to two triangles.
+  /**
+   * Recursively divide all triangles.
+   */
+  function devide_all(recursionDepth, nbRecusions) {
+    // nbRecusions is not set from initial call.
+    if (nbRecusions === undefined) {
+      nbRecusions = 0;
+    }
+    // Stop criterion.
+    if (nbRecusions === recursionDepth) {
+      return;
+    }
 
-					// Remember index of new vertex.
+    const newPolygon = [];
+    for (let vertex of this.polygonVertices) {
+      // get points of triangle
+      const p0 = vertex[0];
+      const p1 = vertex[1];
+      const p2 = vertex[2];
 
-					//console.log("Calculate new vertex "+v+"->"+newIndex[v]+" : "+vertices[p[v]]+" + "+ vertices[p[next]]+" = "+ newVertex);
+      //get vectors
+      const v0 = this.vertices[p0];
+      const v1 = this.vertices[p1];
+      const v2 = this.vertices[p2];
 
-					// Use the existing vertex for the new polygon.
+      //create new vectors
+      const aAdd = v0.map((v, index) => v + v2[index]);
+      const bAdd = v0.map((v, index) => v + v1[index]);
+      const cAdd = v1.map((v, index) => v + v2[index]);
 
-					//console.log("New vertex exists "+v+"->"+newIndex[v]+" : "+this.vertices[p[v]]+" + "+ this.vertices[p[next]]+" = "+ newVertex);
+      const aMul = aAdd.map(a => a * 0.5);
+      const bMul = bAdd.map(b => b * 0.5);
+      const cMul = cAdd.map(c => c * 0.5);
 
-			// Assemble new polygons.
-			// Assure mathematical positive order to keep normals pointing outwards.
-			// Triangle in the center.
+      // normalize
+      const vecNorm = (v) => v.map(el => el / Math.sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]));
 
-			// Triangle in the corners.
+      const aN = vecNorm(aMul);
+      const bN = vecNorm(bMul);
+      const cN = vecNorm(cMul);
 
-				//console.log("Assemble new polygons "+v+" : "+p[v]+" , "+ newIndex[nextButOne]+" , "+ newIndex[v]);
+      //get new indices for vertices
+      const i = this.vertices.length;
+      this.vertices.push(aN, bN, cN);
 
-		// Swap result.
+      // construct new Polygon (triangle)
+      newPolygon.push([p0, i + 1, i], [i + 1, p1, i + 2], [i, i + 1, i + 2], [i, i + 2, p2]);
+    }
+    // Swap result.
+    this.polygonVertices = newPolygon;
+    // Recursion.
+    devide_all.call(this, recursionDepth, nbRecusions + 1);
+  }
 
-		// Recursion.
-
-	}
-	
-	// END exercise Sphere
+  // END exercise Sphere
 
 });
